66d3259c88c2df007de4ece014e08a65b0e61df5
Anomaly369 <damiankofijoseph@gmail.com>
2025-06-13 22:54:46 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+
+self.addEventListener('install', event => {
+  console.log('Auralyn Temple Service Worker installed.');
+});
+
+self.addEventListener('fetch', event => {
+  // Default fetch behavior
+});
