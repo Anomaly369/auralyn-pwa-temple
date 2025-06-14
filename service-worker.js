71c4006576f48c1c2e0c7b38ee24ced65cd7e40c
Anomaly369 <damiankofijoseph@gmail.com>
2025-06-13 22:53:37 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', event => {
-  console.log('Auralyn Temple Service Worker installed.');
-});
-
-self.addEventListener('fetch', event => {
-  // Default fetch behavior
-});
